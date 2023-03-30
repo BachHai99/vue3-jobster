6e5bf67765fdc03a6d76f285c033345c1d3e31ca
Hai Bach <haibach1299@gmail.com>
2023-03-30 17:39:09 +0700

    updated 30/3 pm

--- src/components/Job.vue
+++ src/components/Job.vue
@@ -18,26 +18,17 @@
       </div>
       <footer>
         <div class="actions">
-          <router-link
-            to="/add-job"
-            class="btn edit-btn"
-            @click="
-              storeJob.setEditJob({
-                editJobId: props.job._id,
-                position: props.job.position,
-                company: props.job.company,
-                jobLocation: props.job.jobLocation,
-                jobType: props.job.jobType,
-                status: props.job.status,
-              })
-            "
-            >Edit</router-link
-          >
-          <button
-            type="button"
-            class="btn delete-btn"
-            @click="storeJob.deleteJob(props.job._id)"
-          >
+          <router-link to="/add-job" class="btn edit-btn" @click="
+            storeJob.setEditJob({
+              editJobId: props.job._id,
+              position: props.job.position,
+              company: props.job.company,
+              jobLocation: props.job.jobLocation,
+              jobType: props.job.jobType,
+              status: props.job.status,
+            })
+          ">Edit</router-link>
+          <button type="button" class="btn delete-btn" @click="storeJob.deleteJob(props.job._id)">
             Delete
           </button>
         </div>
@@ -66,6 +57,10 @@ article {
   display: grid;
   grid-template-rows: 1fr auto;
   box-shadow: var(--shadow-2);
+
+  &:hover>.actions {
+    visibility: visible;
+  }
 }
 
 header {
@@ -74,10 +69,12 @@ header {
   display: grid;
   grid-template-columns: auto 1fr;
   align-items: center;
+
   h5 {
     letter-spacing: 0;
   }
 }
+
 .main-icon {
   width: 60px;
   height: 60px;
@@ -91,10 +88,12 @@ header {
   color: var(--white);
   margin-right: 2rem;
 }
+
 .info {
   h5 {
     margin-bottom: 0.25rem;
   }
+
   p {
     margin: 0;
     text-transform: capitalize;
@@ -102,31 +101,39 @@ header {
     letter-spacing: var(--letterSpacing);
   }
 }
+
 .pending {
   background: #fcefc7;
   color: #e9b949;
 }
+
 .interview {
   background: #e0e8f9;
   color: #647acb;
 }
+
 .declined {
   color: #d66a6a;
   background: #ffeeee;
 }
+
 .content {
   padding: 1rem 1.5rem;
 }
+
 .content-center {
   display: grid;
   grid-template-columns: 1fr;
   row-gap: 0.5rem;
+
   @media (min-width: 576px) {
     grid-template-columns: 1fr 1fr;
   }
+
   @media (min-width: 992px) {
     grid-template-columns: 1fr;
   }
+
   @media (min-width: 1120px) {
     grid-template-columns: 1fr 1fr;
   }
@@ -141,25 +148,29 @@ header {
   height: 30px;
   margin-top: 0.5rem;
 }
+
 footer {
   margin-top: 1rem;
 }
+
 .edit-btn,
 .delete-btn {
   letter-spacing: var(--letterSpacing);
   cursor: pointer;
   height: 30px;
 }
+
 .edit-btn {
   color: var(--green-dark);
   background: var(--green-light);
   margin-right: 0.5rem;
 }
+
 .delete-btn {
   color: var(--red-dark);
   background: var(--red-light);
 }
-// &:hover > .actions {
-//   visibility: visible;
-// }
+.actions {
+  float: left;
+}
 </style>
